0d8de2386be59c2ea448a988efd776f5367fbf31
Paul Blackwell <blackwellpaul34@gmail.com>
2021-05-02 11:53:19 +0100

    Updated the weeks collection type

--- api/week/controllers/week.js
+++ api/week/controllers/week.js
@@ -1,36 +1,8 @@
 'use strict';
-const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {
-    async find(ctx) {
-        let entities;
-        if (ctx.request.method === 'GET') {
-            //entities = await strapi.services.restaurant.search(ctx.query);
-            return strapi.query('week').find(ctx.query, [
-                { path: 'workouts', populate: { path: 'warmups', } },
-                { path: 'workouts', populate: { path: 'exercises' } },
-            ]);
-        }
-        // } else if (ctx.request.method === 'POST') {
-        //     //entities = await strapi.services.restaurant.find(ctx.query);
-        //     console.log(ctx.request)
-        // }
-
-        //console.log(ctx.request)
-
-        // return entities.map(entity => {
-        //   const restaurant = sanitizeEntity(entity, {
-        //     model: strapi.models.restaurant,
-        //   });
-        //   if (restaurant.chef && restaurant.chef.email) {
-        //     delete restaurant.chef.email;
-        //   }
-        //   return restaurant;
-        // });
-    },
-};
+module.exports = {};
--- api/week/services/week.js
+++ api/week/services/week.js
@@ -1,16 +1,8 @@
 'use strict';
 
-
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
 
-module.exports = {
-    // find: ctx => {
-    //     return strapi.query('week').find(ctx.query, [
-    //         { path: 'workouts', populate: { path: 'warmups',}},
-    //         { path: 'workouts', populate: { path: 'exercises' }},
-    //     ]);
-    // },
-}
+module.exports = {};
